c1327734321644a9cc9784eb42ca256f11103738
amandapmn <40667726+amandapmn@users.noreply.github.com>
2021-08-25 11:40:33 -0300

    Add files via upload

--- Trab1/src/main/antlr4/br/ufscar/dc/compiladores/trab1/LingA.g4
+++ Trab1/src/main/antlr4/br/ufscar/dc/compiladores/trab1/LingA.g4
@@ -1,11 +1,10 @@
 lexer grammar LingA;
 
-PALAVRA_CHAVE	:	'declare' | 'algoritmo' | 'inteiro' | 'literal' | 'real' | 'logico' | 'atribuir' | 'leia' | 'imprimir' | 'se' | 'entao' 
-	| 'enquanto' | 'inicio' | 'fim_algoritmo' | 'fim_se' | 'registro' | 'fim_registro';
+PALAVRA_CHAVE	:	'declare' | 'algoritmo' | 'inteiro' | 'literal' | 'real' | 'logico' | 'atribuir' | 'leia' | 'imprima' | 'se' | 'entao' | 'enquanto' | 'inicio' | 'fim_algoritmo' | 'fim_se' | 'registro' | 'escreva'| 'fim_registro' | 'caso' | 'fim_caso' | 'seja' | 'senao' | 'faca' | 'ate' | 'para' | 'fim_para' | 'fim_enquanto' | 'retorne' ;
 
-NUMINT	: ('+'|'-')?('0'..'9')+
+NUM_INT	: ('+'|'-')?('0'..'9')+
 	;
-NUMREAL	: ('+'|'-')?('0'..'9')+ ('.' ('0'..'9')+)?
+NUM_REAL	: ('+'|'-')?('0'..'9')+ ('.' ('0'..'9')+)?
 	;
 IDENT : ('a'..'z'|'A'..'Z') ('a'..'z'|'A'..'Z'|'0'..'9')*
 	 ;
@@ -32,5 +31,7 @@ ABREPAR :	'('
 FECHAPAR:	')'
 	;
 VIRGULA: ','
+	;
+INTERVALO: '..'
 	;
 
--- Trab1/src/main/java/Principal.java
+++ Trab1/src/main/java/Principal.java
@@ -1,23 +1,51 @@
 package br.ufscar.dc.compiladores.trab1;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
 
+
 public class Principal {
 
     public static void main(String[] args) {
-                try {
+        try {
             CharStream cs = CharStreams.fromFileName(args[0]);
             LingA lex = new LingA(cs);
             Token t = null;
-            while ((t = lex.nextToken()).getType() != Token.EOF) {
-                System.out.println("<'" + t.getText() +  "'," + LingA.VOCABULARY.getDisplayName(t.getType()) + ">");
+            File f1 = new File("saida.txt");
+            if(f1.createNewFile() == false){
+                f1.delete();
+                f1.createNewFile();
             }
+            while ((t = lex.nextToken()).getType() != Token.EOF) {
+                if (t.getType() == 1){
+                    try {
+                       String data = "<'" + t.getText() +  "','" + t.getText() + "'>" + "\n";
 
+                       FileWriter fileWritter = new FileWriter(f1.getName(),true);
+                       BufferedWriter bw = new BufferedWriter(fileWritter);
+                       bw.write(data);
+                       bw.close();
+                    } catch(IOException e){
+                    }
+                }
+                else{
+                    try {
+                       String data = "<'" + t.getText() +  "'," + LingA.VOCABULARY.getDisplayName(t.getType()) + ">" + "\n";
+                      
+                       FileWriter fileWritter = new FileWriter(f1.getName(),true);
+                       BufferedWriter bw = new BufferedWriter(fileWritter);
+                       bw.write(data);
+                       bw.close();
+                    } catch(IOException e){
+                    }
+                }
+            }
         } catch (IOException ex) {
-        }
-               
+        }        
     }
 }
