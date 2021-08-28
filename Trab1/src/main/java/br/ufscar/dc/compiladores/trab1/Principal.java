65a8a6e7acabb6ad192c3aa464f68522ef4e4c03
amandapmn <40667726+amandapmn@users.noreply.github.com>
2021-08-28 18:14:22 -0300

    Add files via upload

--- Trab1/src/main/java/br/ufscar/dc/compiladores/trab1/Principal.java
+++ Trab1/src/main/java/br/ufscar/dc/compiladores/trab1/Principal.java
@@ -1,11 +1,18 @@
+
+
+
 package br.ufscar.dc.compiladores.trab1;
 
+// Classes para tratamento de arquivos
 import java.io.File;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
+
+// Classes padrão
 import java.io.IOException;
 import java.nio.Buffer;
 
+// Classes para tratamento antlr
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
@@ -13,31 +20,57 @@ import org.antlr.v4.runtime.Token;
 public class Principal {
 
     public static void main(String[] args) throws IOException {
+
+        // Leitura de characteres por arquivo (arquivo no args[0])
         CharStream cs = CharStreams.fromFileName(args[0]);
+        
+        // Variável de tratamento do analizador léxico
         LingA lex = new LingA(cs);
+        
+        // Cada token do léxico da linguagemA ANTLR criado
         Token t = null;
-        int line = 1;
+        
+        // Cria um vetor de escrita de arquivo do arquivo passado em args[1], utilizando o append para arquivos já criados
         try (FileWriter fileWritter = new FileWriter(args[1], true)) {
+            
+        // Cria um buffer de escrita, através do vetor de escrita criado na linha 37
             try (BufferedWriter bw = new BufferedWriter(fileWritter)) {
+                
+                // Enquanto o tipo do próximo token a ser lido não for o token de fim do arquivo, continua o loop
                 while ((t = lex.nextToken()).getType() != Token.EOF) {
+                    
+                    // Modelo padrão de escrita do token: <palavra-chave, palavra-chave>
                     String data = "<'" + t.getText() + "','" + t.getText() + "'>" + "\n";
+                    
+                    // Caso o tipo do token seja diferente de 1 (palavra chave) e menor do que 8 (de operadores ou sinais), 
+                    // utiliza-se o modelo de token: <token, modelo-do-token>
                     if (t.getType() != 1 & t.getType() < 8 ) {
                         data = "<'" + t.getText() + "'," + LingA.VOCABULARY.getDisplayName(t.getType()) + ">" + "\n";
                     }
+                    
+                    // Caso o tipo do token seja nenhum dos identificados anteriormente, ocorreu um erro de simbolo não identificado
+                    // O erro é escrito no arquivo e quebra-se o loop para interromper a execução.
                     if (t.getType() == 19){                        
-                        bw.write("Linha " + line + ": " + t.getText() + " - simbolo nao identificado");
+                        bw.write("Linha " + t.getLine() + ": " + t.getText() + " - simbolo nao identificado");
                         break;
                     }
-                    else if (t.getType() == 18){                        
-                        bw.write("Linha " + line + ": cadeia literal nao fechada");
-                        break;
-                    }
+                    
+                    // Caso o tipo do token seja uma cadeia literal sem fechamento de aspas, reporta-se o erro
+                    // e quebra-se o loop para interromper a execução.
                     else if (t.getType() == 17){                        
-                        bw.write("Linha " + line + ": comentario nao fechado");
+                        bw.write("Linha " + t.getLine() + ": cadeia literal nao fechada");
                         break;
                     }
+                    
+                    // Caso o tipo do token reconhecido seja um comentário sem fechamento, reporta-se
+                    // e quebra-se o loop para interromper a execução do resto do código.
+                    else if (t.getType() == 18){                        
+                        bw.write("Linha " + t.getLine() + ": comentario nao fechado");
+                        break;
+                    }
+                    
+                    // Escrita do token no arquivo, caso não ocorra nenhum problema
                     bw.write(data);
-                    line = line + 1;
                 }
             }
         }
