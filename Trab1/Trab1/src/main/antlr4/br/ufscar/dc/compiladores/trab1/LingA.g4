8e1f1bf94d258900d06082e57ffc1f6f3f4e5a4c
amandapmn <40667726+amandapmn@users.noreply.github.com>
2021-08-29 20:26:34 -0300

    Adicionando comentários

---
+++ Trab1/Trab1/src/main/antlr4/br/ufscar/dc/compiladores/trab1/LingA.g4
@@ -0,0 +1,51 @@
+lexer grammar LingA;
+
+
+
+PALAVRA_CHAVE	:	'verdadeiro'| 'falso'| 'var'|'tipo'|'nao'|'e'|'ou'|'fim_funcao'|'funcao'| 'fim_procedimento'|'procedimento' | 'constante'| 'declare' | 'algoritmo' | 'inteiro' | 'literal' | 'real' | 'logico' | 'atribuir' | 'leia' | 'imprima' | 'se' | 'entao' | 'enquanto' | 'inicio' | 'fim_algoritmo' | 'fim_se' | 'registro' | 'escreva'| 'fim_registro' | 'caso' | 'fim_caso' | 'seja' | 'senao' | 'faca' | 'ate' | 'para' | 'fim_para' | 'fim_enquanto' | 'retorne' ;
+
+NUM_INT	: ('0'..'9')+
+	;
+NUM_REAL	: ('0'..'9')+ ('.' ('0'..'9')+)?
+	;
+IDENT : ('a'..'z'|'A'..'Z') ('a'..'z'|'A'..'Z'|'0'..'9'|'_')*
+	 ;
+
+CADEIA 	: '"' ( ESC_SEQ | ~('\''|'\\'|'\n'|'\r') )+? '"';
+fragment
+ESC_SEQ	: '\\\'';
+	
+COMENTARIO
+    :   '{' ~('}'|'\n'|'\r')* '}' {skip();};
+
+WS  :   ( ' '
+        | '\t'
+        | '\r'
+        | '\n'
+        ) {skip();}
+    ;
+OP_REL	:	'>' | '>=' | '<' | '<=' | '<>' | '=' | '<-' | '&'
+	;
+OP_ARIT	:	'+' | '-' | '*' | '/' | '%' | '^'
+	;
+DELIM	:	':'
+	;
+ABREPAR :	'('
+	;
+FECHAPAR:	')'
+	;
+VIRGULA: ','
+	;
+INTERVALO: '..'
+	;
+PONTO: '.';
+
+ABRE_CHAVE: '[';
+
+FECHA_CHAVE: ']';
+
+CADEIA_ERRADA: ('"') ~('"');
+
+COMENTARIO_ERRADO:   '{' ~('}');
+
+ERRO: .;
---
+++ Trab1/Trab1/src/main/java/br/ufscar/dc/compiladores/trab1/Principal.java
@@ -0,0 +1,77 @@
+package br.ufscar.dc.compiladores.trab1;
+
+// Classes para tratamento de arquivos
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+// Classes padrão
+import java.io.IOException;
+import java.nio.Buffer;
+
+// Classes para tratamento antlr
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
+public class Principal {
+
+    public static void main(String[] args) throws IOException {
+
+        // Leitura de characteres por arquivo (arquivo no args[0])
+        CharStream cs = CharStreams.fromFileName(args[0]);
+        
+        // Variável de tratamento do analizador léxico
+        LingA lex = new LingA(cs);
+        
+        // Variável para cada token do léxico da linguagemA ANTLR criado
+        Token t = null;
+        
+        // Cria um vetor de escrita de arquivo do arquivo passado em args[1], utilizando o append para arquivos já criados
+        // O arquivo passado em args[1] deve ser o arquivo onde os tokens dos lexemas do código compilado serão escritos.
+        try (FileWriter fileWritter = new FileWriter(args[1], true)) {
+            
+        // Cria um buffer de escrita, através do vetor de escrita criado na linha 37
+            try (BufferedWriter bw = new BufferedWriter(fileWritter)) {
+                
+                // Enquanto o tipo do próximo token a ser lido não for o token de fim do arquivo,
+                // continua lendo cada lexema do código de entrada.
+                while ((t = lex.nextToken()).getType() != Token.EOF) {
+                    
+                    // Modelo padrão de produção do token: <palavra-chave, palavra-chave>
+                    String data = "<'" + t.getText() + "','" + t.getText() + "'>" + "\n";
+                    
+                    // Caso o tipo do token seja diferente de 1 (palavra chave) e menor do que 8 (de operadores ou sinais), 
+                    // utiliza-se o modelo de token: <token, modelo-do-token>
+                    if (t.getType() != 1 & t.getType() < 8) {
+                        data = "<'" + t.getText() + "'," + LingA.VOCABULARY.getDisplayName(t.getType()) + ">" + "\n";
+                    }
+                    
+                    // Caso o tipo do token seja uma cadeia literal sem fechamento de aspas, reporta-se o erro
+                    // e quebra-se o loop para interromper a execução.
+                    if (t.getType() == 18){                        
+                        bw.write("Linha " + t.getLine() + ": cadeia literal nao fechada" + "\n");
+                        break;
+                    }
+                    
+                    // Caso o tipo do token reconhecido seja um comentário sem fechamento, reporta-se
+                    // e quebra-se o loop para interromper a execução do resto do código.
+                    else if (t.getType() == 19){                        
+                        bw.write("Linha " + t.getLine() + ": comentario nao fechado" + "\n");
+                        break;
+                    }
+                    
+                    // Caso o tipo do token seja nenhum dos identificados anteriormente, ocorreu um erro de simbolo não identificado
+                    // O erro é escrito no arquivo e quebra-se o loop para interromper a execução.
+                    else if (t.getType() == 20){
+                        bw.write("Linha " + t.getLine() + ": " + t.getText() + " - simbolo nao identificado" + "\n");
+                        break;
+                    }
+                    
+                    // Escrita do token no arquivo, caso não ocorra nenhum problema
+                    bw.write(data);
+                }
+            }
+        }
+    }
+}
